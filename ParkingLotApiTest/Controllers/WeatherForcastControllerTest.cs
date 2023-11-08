d0c7bbf9188a9ef7ad0abd6c2f42b171381cc3fd
Wency3 <XWen3@slb.com>
2023-11-08 13:22:22 +0800

    refactor: extract TestBase for API test.

--- ParkingLotApiTest/Controllers/WeatherForcastControllerTest.cs
+++ ParkingLotApiTest/Controllers/WeatherForcastControllerTest.cs
@@ -3,24 +3,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ParkingLotApiTest.Controllers
 {
-    public class WeatherForcastControllerTest
+    public class WeatherForcastControllerTest : TestBase
     {
-        private HttpClient _httpClient;
-        public WeatherForcastControllerTest()
-        {
-            WebApplicationFactory<Program> webApplicationFactory = new WebApplicationFactory<Program>();
-            _httpClient = webApplicationFactory.CreateClient();
+        public WeatherForcastControllerTest(WebApplicationFactory<Program> factory) : base(factory)
+        { 
+
         }
 
         [Fact]
         public async Task Should_return_correctly_when_get_weather_forecast()
         {
-            HttpResponseMessage response = await _httpClient.GetAsync("/WeatherForecast");
+            HttpClient httpClient = GetClient();
+            HttpResponseMessage response = await httpClient.GetAsync("/WeatherForecast");
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         }
     }
