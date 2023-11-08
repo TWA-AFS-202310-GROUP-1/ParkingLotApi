75f1ba8a76ac8130c59fdc887bf6edfb64591b0b
Tianwei Liu <TLiu25@slb.com>
2023-11-08 13:35:53 +0800

    refactor: test refactor

---
+++ ParkingLotApiTest/TestBase.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using ParkingLotApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLotApiTest
+{
+    public class TestBase : IClassFixture<WebApplicationFactory<Program>>
+    {
+        public TestBase(WebApplicationFactory<Program> factory) 
+        {
+            this.Factory = factory;
+        }
+
+        public WebApplicationFactory<Program> Factory { get; }
+
+        public HttpClient GetClient()
+        {
+            return Factory.CreateClient();
+        }
+    }
+}
